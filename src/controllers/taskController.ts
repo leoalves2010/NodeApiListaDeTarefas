25e4366d5e124bab38c02ca376d4efcd1b15f104
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-05-02 21:02:22 -0300

    Criação do Controller 'createTask' para adição de uma NOVA tarefa

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -30,23 +30,23 @@ export const getAllTasks = async (req: Request, res: Response) => {
                     ]
                 })
             }
-            res.json({tasks}).status(200);
+            res.status(200).json({tasks});
         } catch (error) {
-            res.json({error}).status(500);
+            res.status(500).json({error});
         }
     }else{
-        res.json({
+        res.status(404).json({
             tasks: {
                 msg: "There are no tasks to show."
             },
             links: [
                 {
                     type: "POST",
-                    rel: "add_tasks",
+                    rel: "add_task",
                     uri: 'http://localhost:4000/task'
                 }
             ]
-        }).status(404);
+        });
     }
 }
 
@@ -55,7 +55,50 @@ export const getOneTask = async (req: Request, res: Response) => {
 }
 
 export const createTask = async (req: Request, res: Response) => {
-    
+    if(req.body.title){
+        try {
+            let newTask = await Task.create({
+                title: req.body.title,
+                done: req.body.done ? 1 : 0
+            });
+            res.status(201).json({
+                msg: "Task created successfully.",
+                newTask,
+                links: [
+                    {
+                        type: "GET",
+                        rel: "get_one_task_by_id",
+                        uri: `http://localhost:4000/task/${newTask.id}`
+                    },
+                    {
+                        type: "PUT",
+                        rel: "update_task",
+                        uri: `http://localhost:4000/task/${newTask.id}`
+                    },
+                    {
+                        type: "DELETE",
+                        rel: "delete_task",
+                        uri: `http://localhost:4000/task/${newTask.id}`
+                    }
+                ]
+            });
+        } catch (error) {
+            res.status(500).json({error});
+        }
+    }else{
+        res.status(422).json({
+            tasks: {
+                msg: "Error. Fill in the data correctly."
+            },
+            links: [
+                {
+                    type: "POST",
+                    rel: "add_task",
+                    uri: 'http://localhost:4000/task'
+                }
+            ]
+        });
+    }
 }
 
 export const updateTask = async (req: Request, res: Response) => {
